48f5760771255b859864fdbfdedc2b39e140cb88
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-28 03:13:34 -0500

    Initial inline implementation to open the specified Trello Board.

--- lib/nutrella/command.rb
+++ lib/nutrella/command.rb
@@ -1,13 +1,43 @@
+require "trello"
+require "yaml"
+
 module Nutrella
   class Command
     def initialize(args)
       @options = Options.new(args)
+      @member_id = "amckinnell"
     end
 
     def run
-      puts "Board name: #{@options.board_name}" if @options.parse
+      open_board if @options.parse
     rescue OptionParser::InvalidOption => e
       puts e
+    end
+
+    private
+
+    def open_board
+      configure_trello
+      system "open #{find_board.url}"
+    end
+
+    def configure_trello
+      trello_keys = YAML.load_file("trello_keys.yml")
+
+      Trello.configure do |config|
+        config.consumer_key = trello_keys.fetch(:key)
+        config.consumer_secret = trello_keys.fetch(:secret)
+        config.oauth_token = trello_keys.fetch(:token)
+        config.oauth_token_secret = trello_keys.fetch(:secret)
+      end
+    end
+
+    def find_board
+      member.boards.find { |board| board.name == @options.board_name }
+    end
+
+    def member
+      Trello::Member.find(@member_id)
     end
   end
 end
