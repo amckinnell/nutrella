b479e2a7d5ca58655c594e16c5d325f5cbb2c0b1
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-27 20:27:06 -0500

    Add primitive argument parsing.

--- lib/nutrella/command.rb
+++ lib/nutrella/command.rb
@@ -1,4 +1,6 @@
 require "git"
+require "optionparser"
+require "ostruct"
 
 module Nutrella
   class Command
@@ -9,10 +11,31 @@ module Nutrella
     end
 
     def run
-      puts "Arguments: #{ARGV}"
-
       g = Git.open(".")
       puts "Current branch: #{g.current_branch}"
+
+      puts "Options: #{parse(ARGV)}"
     end
+
+    private
+
+    # rubocop:disable Metrics/MethodLength
+    def parse(args)
+      options = OpenStruct.new
+
+      OptionParser.new do |opts|
+        opts.on("-t", "--trello-board BOARD", "Open the Trello Board with name BOARD") do |t|
+          options.board_name = t
+        end
+
+        opts.on("-h", "--help", "Display this screen") do
+          puts(opts)
+          exit
+        end
+      end.parse!(args)
+
+      options
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
