d62c4175c1e8dc7319bb4abe5533631ceea7444c
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-28 03:32:06 -0500

    Extract the Board class.

--- lib/nutrella.rb
+++ lib/nutrella.rb
@@ -1,3 +1,4 @@
+require "nutrella/board"
 require "nutrella/command"
 require "nutrella/options"
 require "nutrella/version"
---
+++ lib/nutrella/board.rb
@@ -0,0 +1,35 @@
+require "trello"
+require "yaml"
+
+module Nutrella
+  class Board
+    def initialize(options)
+      @board_name = options.board_name
+      @member_id = "amckinnell"
+    end
+
+    def open_board
+      configure_trello
+      system "open #{find_board.url}"
+    end
+
+    def configure_trello
+      trello_keys = YAML.load_file("trello_keys.yml")
+
+      Trello.configure do |config|
+        config.consumer_key = trello_keys.fetch(:key)
+        config.consumer_secret = trello_keys.fetch(:secret)
+        config.oauth_token = trello_keys.fetch(:token)
+        config.oauth_token_secret = trello_keys.fetch(:secret)
+      end
+    end
+
+    def find_board
+      member.boards.find { |board| board.name == @board_name }
+    end
+
+    def member
+      Trello::Member.find(@member_id)
+    end
+  end
+end
