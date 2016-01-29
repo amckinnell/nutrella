6469790fa51f03a41da69f996e382e5ebc1ae006
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-28 20:19:35 -0500

    Standardize syntax.

--- spec/nutrella_spec.rb
+++ spec/nutrella_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 RSpec.describe Nutrella do
   it "has a version number" do
-    expect(Nutrella::VERSION).not_to be nil
+    expect(Nutrella::VERSION).not_to be(nil)
   end
 end
