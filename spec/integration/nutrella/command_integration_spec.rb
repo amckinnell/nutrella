6cdf9dc0734825afa0882e042f9b909f6c5859e2
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-29 01:33:13 -0500

    Implement a basic integration test.

--- spec/integration/nutrella/command_integration_spec.rb
+++ spec/integration/nutrella/command_integration_spec.rb
@@ -2,6 +2,28 @@ module Nutrella
   vcr_options = { cassette_name: "command", record: :new_episodes }
 
   RSpec.describe Command, vcr: vcr_options do
-    pending
+    it "finds an existing board" do
+      task_board_name = "Nutrella"
+      subject = command("-t", task_board_name)
+
+      task_board = subject.run
+
+      expect(task_board).to have_attributes(name: "Nutrella")
+    end
+
+    it "creates a new board" do
+      task_board_name = "nutrella_new_task_board"
+      subject = command("-t", task_board_name)
+
+      allow(subject).to receive(:confirm_create?).and_return("yes")
+
+      task_board = subject.run
+
+      expect(task_board).to have_attributes(name: task_board_name)
+    end
+
+    def command(*args)
+      Command.new(args)
+    end
   end
 end
