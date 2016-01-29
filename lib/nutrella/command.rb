aff35135df31ab64018461bc61338de5f44cc178
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-29 01:10:14 -0500

    Move responsibility for opening the task board to the nutrella executable.

--- lib/nutrella/command.rb
+++ lib/nutrella/command.rb
@@ -6,30 +6,26 @@ module Nutrella
 
     def run
       @options.parse
-      open_board unless @options.show_usage?
+      find_or_create_task_board unless @options.show_usage?
     rescue OptionParser::InvalidOption => e
       puts e
     end
 
     private
 
-    def open_board
+    def find_or_create_task_board
       task_board = TaskBoard.new(@options)
 
       if task_board.exists?
-        open_trello_board task_board.find
+        task_board.find
       elsif confirm_create? task_board
-        open_trello_board task_board.create
+        task_board.create
       end
     end
 
     def confirm_create?(board)
       print "Create the '#{board.name}' task board? [y/N]: "
       gets =~ /^y/i
-    end
-
-    def open_trello_board(board)
-      system "open #{board.url}"
     end
   end
 end
