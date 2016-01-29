4ae867fd83ef34ca3ab6ac93fa1a48431c401b91
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-01-29 00:37:31 -0500

    Add the vcr gem for testing.

--- nutrella.gemspec
+++ nutrella.gemspec
@@ -28,4 +28,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "simplecov", "~> 0.11"
+  spec.add_development_dependency "vcr", "~> 3.0"
+  spec.add_development_dependency "webmock", "~> 1.22"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "nutrella"
+
+require "vcr"
+
+VCR.configure do |config|
+  config.cassette_library_dir = "vcr_cassettes"
+  config.hook_into :webmock
+
+  # Use a VCR cassette for any RSpec example tagged with `:vcr`.
+  config.configure_rspec_metadata!
+end
