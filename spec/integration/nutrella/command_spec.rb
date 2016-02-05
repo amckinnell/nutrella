dc97b423ccda6db4dc843c438a690ae9b7d2fa3b
Jason CheongKeeYou <jasonc@nulogy.com>
2016-02-05 16:06:17 -0500

    Consolidates command specs into single integration test file.

--- spec/integration/nutrella/command_spec.rb
+++ spec/integration/nutrella/command_spec.rb
@@ -20,6 +20,12 @@ module Nutrella
       expect(task_board).to have_attributes(url: match(/https:.*fresh-task-board/))
     end
 
+    it "fails when options don't parse" do
+      expect { Command.new(["--invalid-option"]).task_board }.to(
+        output(/Error: invalid option/).to_stderr.and(raise_error(SystemExit))
+      )
+    end
+
     def command(*args)
       Command.new(args)
     end
--- spec/unit/nutrella/command_spec.rb
+++
@@ -1,9 +0,0 @@
-module Nutrella
-  RSpec.describe Command do
-    it "fails when options don't parse" do
-      expect { Command.new(["--invalid-option"]).task_board }.to(
-        output(/Error: invalid option/).to_stderr.and(raise_error(SystemExit))
-      )
-    end
-  end
-end
