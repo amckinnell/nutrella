37428d1ec86155b608488fc86d13bc69f7301547
Jason CheongKeeYou <jasonc@nulogy.com>
2016-02-05 16:22:43 -0500

    Changes Command tests to call #run rather than #task_board.

--- spec/integration/nutrella/command_spec.rb
+++ spec/integration/nutrella/command_spec.rb
@@ -5,23 +5,22 @@ module Nutrella
     it "finds an existing board" do
       subject = command("-t", "Nutrella")
 
-      task_board = subject.task_board
+      expect(subject).to receive(:system).with(match(%r{open https://trello.com/b/.*/nutrella}))
 
-      expect(task_board).to have_attributes(url: "https://trello.com/b/bqxSqUDM/nutrella")
+      subject.run
     end
 
     it "creates a new board" do
       subject = command("-t", "fresh_task_board")
-
       allow(subject).to receive(:confirm_create?).and_return(true)
 
-      task_board = subject.task_board
+      expect(subject).to receive(:system).with(match(%r{open https://trello.com/b/.*/fresh-task-board}))
 
-      expect(task_board).to have_attributes(url: match(/https:.*fresh-task-board/))
+      subject.run
     end
 
     it "fails when options don't parse" do
-      expect { Command.new(["--invalid-option"]).task_board }.to(
+      expect { Command.new(["--invalid-option"]).run }.to(
         output(/Error: invalid option/).to_stderr.and(raise_error(SystemExit))
       )
     end
