ea13e9d3e262ed5db7e40e9bbc71d69afca7c881
Jason CheongKeeYou <jasonc@nulogy.com>
2016-02-05 20:11:19 -0500

    Changes -t option to only support find, not create.

--- spec/integration/nutrella/command_spec.rb
+++ spec/integration/nutrella/command_spec.rb
@@ -3,30 +3,17 @@ module Nutrella
 
   RSpec.describe "Nutrella", vcr: vcr_options do
     it "displays help" do
-      expect { command("-h").run }.to(
-        output(/Usage:/).to_stdout.and(raise_error(SystemExit))
-      )
+      expect { command("-h").run }.to(output(/Usage:/).to_stdout)
     end
 
     it "displays version" do
-      expect { command("-v").run }.to(
-        output(/#{Nutrella::VERSION}/).to_stdout.and(raise_error(SystemExit))
-      )
+      expect { command("-v").run }.to(output(/#{Nutrella::VERSION}/).to_stdout)
     end
 
     it "finds an existing board" do
       subject = command("-t", "Nutrella")
 
       expect(subject).to receive(:system).with(match(%r{open https://trello.com/b/.*/nutrella}))
-
-      subject.run
-    end
-
-    it "creates a new board" do
-      subject = command("-t", "fresh_task_board")
-      allow(subject).to receive(:confirm_create?).and_return(true)
-
-      expect(subject).to receive(:system).with(match(%r{open https://trello.com/b/.*/fresh-task-board}))
 
       subject.run
     end
