3de0616cb3be8e1263480d7d09470a62bb36a14d
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-02-07 12:38:09 -0500

    Use key/value test literals.

--- spec/unit/nutrella/cache_spec.rb
+++ spec/unit/nutrella/cache_spec.rb
@@ -2,47 +2,47 @@ module Nutrella
   RSpec.describe Cache do
     describe "#get" do
       it "cache hit" do
-        cache_contents(["b 1", "url 1"])
-        expected_cache(["b 1", "url 1"])
+        cache_contents(["k 1", "v 1"])
+        expected_cache(["k 1", "v 1"])
 
-        expect(subject.get("b 1") { "url 1" }).to eq("url 1")
+        expect(subject.get("k 1") { "v 1" }).to eq("v 1")
       end
 
       it "cache miss" do
-        cache_contents(["b 1", "url 1"])
-        expected_cache(["b 2", "url 2"], ["b 1", "url 1"])
+        cache_contents(["k 1", "v 1"])
+        expected_cache(["k 2", "v 2"], ["k 1", "v 1"])
 
-        expect(subject.get("b 2") { "url 2" }).to eq("url 2")
+        expect(subject.get("k 2") { "v 2" }).to eq("v 2")
       end
     end
 
     describe "#put" do
       it "add to top of empty cache" do
         cache_contents
-        expected_cache(["b 1", "url 1"])
+        expected_cache(["k 1", "v 1"])
 
-        expect(subject.put("b 1") { "url 1" }).to eq("url 1")
+        expect(subject.put("k 1") { "v 1" }).to eq("v 1")
       end
 
       it "add to top of cache" do
-        cache_contents(["b 2", "url 2"])
-        expected_cache(["b 1", "url 1"], ["b 2", "url 2"])
+        cache_contents(["k 2", "v 2"])
+        expected_cache(["k 1", "v 1"], ["k 2", "v 2"])
 
-        expect(subject.put("b 1") { "url 1" }).to eq("url 1")
+        expect(subject.put("k 1") { "v 1" }).to eq("v 1")
       end
 
       it "shuffle to top of cache" do
-        cache_contents(["b 2", "url 2"], ["b 1", "url 1"], ["b 3", "url 3"])
-        expected_cache(["b 1", "url 1"], ["b 2", "url 2"], ["b 3", "url 3"])
+        cache_contents(["k 2", "v 2"], ["k 1", "v 1"], ["k 3", "v 3"])
+        expected_cache(["k 1", "v 1"], ["k 2", "v 2"], ["k 3", "v 3"])
 
-        expect(subject.put("b 1") { "url 1" }).to eq("url 1")
+        expect(subject.put("k 1") { "v 1" }).to eq("v 1")
       end
 
       it "enforce cache capacity" do
-        cache_contents(["b 1", "url 1"], ["b 2", "url 2"], ["b 3", "url 3"], ["b 4", "url 4"], ["b 5", "url 5"])
-        expected_cache(["b 6", "url 6"], ["b 1", "url 1"], ["b 2", "url 2"], ["b 3", "url 3"], ["b 4", "url 4"])
+        cache_contents(["k 1", "v 1"], ["k 2", "v 2"], ["k 3", "v 3"], ["k 4", "v 4"], ["k 5", "v 5"])
+        expected_cache(["k 6", "v 6"], ["k 1", "v 1"], ["k 2", "v 2"], ["k 3", "v 3"], ["k 4", "v 4"])
 
-        expect(subject.put("b 6") { "url 6" }).to eq("url 6")
+        expect(subject.put("k 6") { "v 6" }).to eq("v 6")
       end
     end
 
