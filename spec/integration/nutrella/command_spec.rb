97a2968b9cbdb746bd57119d13d83f85be30ce14
Jason CheongKeeYou <jasonc@nulogy.com>
2016-02-08 16:19:50 -0500

    Removes support for -h option.

--- spec/integration/nutrella/command_spec.rb
+++ spec/integration/nutrella/command_spec.rb
@@ -2,10 +2,6 @@ module Nutrella
   vcr_options = { cassette_name: "nutrella", record: :new_episodes }
 
   RSpec.describe Command, vcr: vcr_options do
-    it "displays help" do
-      expect { subject.run(["-h"]) }.to output(/Usage:/).to_stdout
-    end
-
     it "finds an existing board" do
       disable_cache
 
