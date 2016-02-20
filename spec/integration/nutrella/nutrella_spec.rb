c1e9dc484ef0bf308e858e5a81b3ced7fa65aa50
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-02-20 15:06:13 -0500

    Completes the nutrella specification.

--- spec/integration/nutrella/nutrella_spec.rb
+++ spec/integration/nutrella/nutrella_spec.rb
@@ -10,10 +10,23 @@ module Nutrella
       end
     end
 
+    it "looks up an existing task board" do
+      Dir.mktmpdir do |home_dir|
+        create_configuration_file(home_dir)
+        arrange_trello_for_lookup(board_name: "My Board", url: "board_url")
+
+        subject = Command.new(home_dir)
+
+        expect(subject).to receive(:system).with("open board_url")
+
+        subject.run
+      end
+    end
+
     it "creates a task board" do
       Dir.mktmpdir do |home_dir|
         create_configuration_file(home_dir)
-        arrange_trello(board_name: "My Board", url: "board_url")
+        arrange_trello_for_create(board_name: "My Board", url: "board_url")
 
         subject = Command.new(home_dir)
 
@@ -27,10 +40,27 @@ module Nutrella
       File.write(configuration_filename(home_dir), sample_configuration)
     end
 
-    def arrange_trello(board_name:, url:)
-      allow(TaskBoardName).to receive(:current_git_branch).and_return(board_name)
-      allow(Trello::Action).to receive(:search).with(board_name, anything)
-        .and_return("boards" => [OpenStruct.new(name: board_name, url: url)])
+    def arrange_trello_for_lookup(board_name:, url:)
+      allow(TaskBoardName).to receive(:current_git_branch)
+        .and_return(board_name)
+
+      expect(Trello::Action).to receive(:search)
+        .with(board_name, anything)
+        .and_return("boards" => [board(id: "lookup_id", name: board_name, url: url)])
+    end
+
+    def arrange_trello_for_create(board_name:, url:)
+      allow(TaskBoardName).to receive(:current_git_branch)
+        .and_return(board_name)
+      allow(Trello::Action).to receive(:search)
+        .with(board_name, anything)
+        .and_return("boards" => [])
+
+      expect(Trello::Board).to receive(:create)
+        .with(name: board_name, organization_id: TaskBoard::NULOGY_ORGANIZATION_ID)
+        .and_return(board(id: "create_id", name: board_name, url: url))
+      expect_any_instance_of(Trello::Client).to receive(:put)
+        .with("/boards/create_id", "prefs/permissionLevel=org")
     end
 
     def expect_contents(configuration_filename, expected_configuration)
@@ -40,6 +70,10 @@ module Nutrella
 
     def configuration_filename(home_dir)
       File.join(home_dir, ".nutrella.yml")
+    end
+
+    def board(id:, name:, url:)
+      OpenStruct.new(id: id, name: name, url: url)
     end
 
     def initial_configuration
