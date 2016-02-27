145124ff91c9c979cb78d2b5a36f922e47e0e269
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-02-27 12:53:13 -0500

    Fail specs which run for longer than 2 seconds. Most useful during mutation testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
+# Right at the top so we don't miss any opportunities to track coverage.
 require "simplecov"
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "nutrella"
+
+# Fail specs which run for longer than 2 seconds. Most useful during mutation testing.
+require "timeout"
+RSpec.configure do |rspec|
+  rspec.around(:each) do |example|
+    Timeout.timeout(2_000, &example)
+  end
+end
