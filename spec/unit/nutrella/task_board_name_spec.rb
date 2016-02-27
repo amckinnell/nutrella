70ef98fb21dd5538bdb548fbd3a4c1b54819154c
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-02-27 18:39:55 -0500

    Avoid #receive_message_chain by returning a test double instead.
    Note: reduces the mutations that survive.

--- spec/unit/nutrella/task_board_name_spec.rb
+++ spec/unit/nutrella/task_board_name_spec.rb
@@ -1,7 +1,8 @@
 module Nutrella
   RSpec.describe TaskBoardName do
     it "derives the task board name from the current git branch" do
-      allow(Git).to receive_message_chain(:open, :current_branch).and_return("1234_feature_branch")
+      allow(Git).to receive(:open).with(".").and_return(
+        instance_double(Git::Base, current_branch: "1234_feature_branch"))
 
       expect(TaskBoardName.from_git_branch(".")).to eq("1234 Feature Branch")
     end
