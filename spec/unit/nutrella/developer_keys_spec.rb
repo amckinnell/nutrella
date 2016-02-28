d1c98698c0aff20047a4a6bb2a91954f5065ffac
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-02-28 06:24:13 -0500

    Simplify.

--- spec/unit/nutrella/developer_keys_spec.rb
+++ spec/unit/nutrella/developer_keys_spec.rb
@@ -2,10 +2,8 @@ module Nutrella
   extend DeveloperKeys
 
   RSpec.describe DeveloperKeys do
-    let(:nutrella) { Nutrella.new { extend DeveloperKeys } }
-
     describe "#open_public_key_url" do
-      it "show Nutrella as the application requesting access in the Trello oauth page" do
+      it "delegate to Trello's #open_public_key_url" do
         expect(Trello).to receive(:open_public_key_url)
 
         Nutrella.open_public_key_url
@@ -13,13 +11,7 @@ module Nutrella
     end
 
     describe "#open_authorization_url" do
-      it "show Nutrella as the application requesting access in the Trello oauth page" do
-        expect(Trello).to receive(:open_authorization_url).with(name: "Nutrella")
-
-        Nutrella.open_authorization_url
-      end
-
-      it "merge application name into existing options" do
+      it "delegate to Trello's #open_authorization_url with Nutrella as the requesting application" do
         expect(Trello).to receive(:open_authorization_url).with(expiration: "1day", name: "Nutrella")
 
         Nutrella.open_authorization_url(expiration: "1day")
