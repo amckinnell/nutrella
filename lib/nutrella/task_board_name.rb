84a064de3620d101018c58db150896e3706166c7
Alistair McKinnell <alistair.mckinnell@gmail.com>
2016-02-28 14:12:34 -0500

    Defaults the working directory to the current directory.

--- lib/nutrella/task_board_name.rb
+++ lib/nutrella/task_board_name.rb
@@ -5,8 +5,8 @@ module Nutrella
   # Knows the name of the task board associated with the current git branch.
   #
   module TaskBoardName
-    def self.from_git_branch(dir)
-      Git.open(dir).current_branch.titleize
+    def self.from_git_branch(working_dir = ".")
+      Git.open(working_dir).current_branch.titleize
     rescue
       abort "Sorry. Can't find an associated git branch here."
     end
--- spec/unit/nutrella/task_board_name_spec.rb
+++ spec/unit/nutrella/task_board_name_spec.rb
@@ -1,10 +1,17 @@
 module Nutrella
   RSpec.describe TaskBoardName do
-    it "derives the task board name from the current git branch" do
-      allow(Git).to receive(:open).with(".").and_return(
-        instance_double(Git::Base, current_branch: "1234_feature_branch"))
+    describe "derives the task board name from the current git branch" do
+      it "given a directory" do
+        configure_git(working_dir: "dir", current_branch: "master")
 
-      expect(TaskBoardName.from_git_branch(".")).to eq("1234 Feature Branch")
+        expect(TaskBoardName.from_git_branch("dir")).to eq("Master")
+      end
+
+      it "defaulting to the current directory" do
+        configure_git(working_dir: ".", current_branch: "1234_feature_branch")
+
+        expect(TaskBoardName.from_git_branch).to eq("1234 Feature Branch")
+      end
     end
 
     it "displays an error when there is no associated git branch" do
@@ -12,6 +19,11 @@ module Nutrella
         expect { TaskBoardName.from_git_branch(non_git_dir) }.to output(
           /Can't find an associated git branch/).to_stderr.and(raise_error(SystemExit))
       end
+    end
+
+    def configure_git(working_dir:, current_branch:)
+      allow(Git).to receive(:open).with(working_dir).and_return(
+        instance_double(Git::Base, current_branch: current_branch))
     end
   end
 end
