4827b5204c70076c358db90078d337cc98f1f9c7
Alistair McKinnell <alistair.mckinnell@gmail.com>
2017-01-02 23:20:22 -0500

    Add simpler versions of #strip_heredoc and #titleize from ActiveSupport.

---
+++ spec/unit/nutrella/string_ext_spec.rb
@@ -0,0 +1,28 @@
+module Nutrella
+  RSpec.describe String do
+    describe "#strip_heredoc" do
+      it "removes leading whitespace" do
+        sample = <<-SAMPLE
+          line 1
+          line 2
+        SAMPLE
+
+        expect(sample.strip_heredoc).to eq("line 1\nline 2\n")
+      end
+    end
+
+    describe "#titleize" do
+      it "removes dashes" do
+        git_branch_name = "1234-performance-fix"
+
+        expect(git_branch_name.titleize).to eq("1234 Performance Fix")
+      end
+
+      it "removes underscores" do
+        git_branch_name = "1234_performance_fix"
+
+        expect(git_branch_name.titleize).to eq("1234 Performance Fix")
+      end
+    end
+  end
+end
