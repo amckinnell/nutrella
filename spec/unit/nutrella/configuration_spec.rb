7060917e57032d471d5712a42b3d4431f6f3c456
Alistair McKinnell <alistairm@nulogy.com>
2019-05-20 16:38:54 -0400

    Extract the configuration values to a let

--- spec/unit/nutrella/configuration_spec.rb
+++ spec/unit/nutrella/configuration_spec.rb
@@ -1,12 +1,13 @@
 RSpec.describe Nutrella::Configuration do
   let(:path) { "path" }
+  let(:values) { { key: "c1", secret: "5f", token: "3c", organization: "org" } }
 
   subject(:configuration) { Nutrella::Configuration.values(path) }
 
   it "succeeds when the configuration exists and is well formed" do
-    configuration_file_contents(key: "c1", secret: "5f", token: "3c", organization: "org")
+    configuration_file_contents(values)
 
-    expect(configuration).to eq(key: "c1", secret: "5f", token: "3c", organization: "org")
+    expect(configuration).to include(values)
   end
 
   it "handles the case when the configuration is missing" do
@@ -19,7 +20,7 @@ RSpec.describe Nutrella::Configuration do
   end
 
   it "fails when configuration is malformed (missing secret)" do
-    configuration_file_contents(key: "c1", token: "5f", organization: "org")
+    configuration_file_contents(values.except(:secret))
 
     expect { configuration }.to output(/#{path} key not found: "secret"/)
       .to_stderr.and(raise_error(SystemExit))
@@ -27,7 +28,7 @@ RSpec.describe Nutrella::Configuration do
 
   def configuration_file_contents(values)
     allow(File).to receive(:exist?).with(path).and_return(true)
-    allow(YAML).to receive(:load_file).with(path).and_return(values.stringify_keys)
+    allow(YAML).to receive(:load_file).with(path).and_return(values.transform_keys(&:to_s))
   end
 
   def configuration_file_missing
