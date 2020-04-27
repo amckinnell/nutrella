6f7bab3c5d2d63bc83b8d8d33b5546d48ec76732
Alistair McKinnell <alistairm@nulogy.com>
2020-04-27 05:51:29 -0400

    Honour the specified named Trello board argument first
    - Choose the specified argument over the current git branch

--- lib/nutrella/task_board_name.rb
+++ lib/nutrella/task_board_name.rb
@@ -10,11 +10,11 @@ module Nutrella
   #
   class TaskBoardName
     def self.board_name(args)
-      git_branch_name, status = Open3.capture2("git rev-parse --abbrev-ref HEAD")
-      return git_branch_name.chomp if status.success?
-
       git_branch_name = args[0]
       return git_branch_name if git_branch_name.present?
+
+      git_branch_name, status = Open3.capture2("git rev-parse --abbrev-ref HEAD")
+      return git_branch_name.chomp if status.success?
 
       abort "Sorry. Can't figure out a name for the board."
     end
