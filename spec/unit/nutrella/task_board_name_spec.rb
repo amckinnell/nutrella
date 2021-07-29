6c041aa006de001e8203289331e9b9654365e02e
Alistair McKinnell <alistairm@nulogy.com>
2021-07-29 15:44:49 -0400

    Rename board_name to board_name_from_git_branch

--- spec/unit/nutrella/task_board_name_spec.rb
+++ spec/unit/nutrella/task_board_name_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Nutrella::TaskBoardName do
   it "derives the task board name from the first command line arg" do
     configure_git_command_failure
 
-    expect(task_board_name.board_name(["command_line"])).to eq("command_line")
+    expect(task_board_name.board_name_from_git_branch(["command_line"])).to eq("command_line")
   end
 
   it "derives the task board name from the current git branch" do
     configure_git_command_success(current_branch: "git_branch")
 
-    expect(task_board_name.board_name([])).to eq("git_branch")
+    expect(task_board_name.board_name_from_git_branch([])).to eq("git_branch")
   end
 
   it "displays an error when there is no command line arg and no current git branch" do
     configure_git_command_failure
 
-    expect { task_board_name.board_name([]) }.to output(
+    expect { task_board_name.board_name_from_git_branch([]) }.to output(
       "Sorry. Can't figure out a name for the board.\n"
     ).to_stderr.and(raise_error(SystemExit))
   end
